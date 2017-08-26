7fa0b6f732fabcbb0986c1dae0416cab8559a6fe
Michael J Raymond <michaeljohnraymond@gmail.com>
2017-08-25 23:40:30 -0500

    added dummy http response

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TodoList
+Django tutorial [here](https://www.youtube.com/watch?v=2yXfUPwlZTw)
--- todolist/urls.py
+++ todolist/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(todos.urls)),
+    url(r'^$', include('todos.urls')),
+    url(r'^todos/', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
